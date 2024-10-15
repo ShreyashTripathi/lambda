91a1eb2b6ed78c9d9c8e2f414805b4f0e74e2887
shreyash <shreyash@convegenius.ai>
2024-10-15 16:33:18 +0530

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda new version 15 oct 16:15!'),
+      body: JSON.stringify('Hello from Lambda new version 15 oct 16:35!'),
     };
     return response;
   };
